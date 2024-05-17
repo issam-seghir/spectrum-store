26e6093a906cb9d824f63d618b82bec6cdf02e72
issam-seghir <issamusma@hotmail.com>
2024-05-17 16:12:53 +0100

    config tailwind them + add header navbar

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,66 @@
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+
+import { ModeToggle } from "@/components/mode-toggler";
+
+export function Navbar() {
+    return (
+        <header className="sticky top-0 z-30 flex items-center justify-between gap-4 border-b bg-background-secondary px-4  py-4 sm:static sm:h-auto  sm:px-6">
+            <h1 className="text-3xl font-bold">
+                <Link href="/">Spectrum</Link>
+            </h1>
+
+            <div>
+                <div className="relative ml-auto flex-1 md:grow-0">
+                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input
+                        type="search"
+                        placeholder="Search..."
+                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+                    />
+                </div>
+            </div>
+            <div className="flex items-center gap-4">
+				<ModeToggle />
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button
+							variant="outline"
+							size="icon"
+							className="overflow-hidden rounded-full"
+						>
+							<Image
+								src="https://i.imgur.com/LFpAx5i.png"
+								width={36}
+								height={36}
+								alt="Avatar"
+								className="overflow-hidden rounded-full"
+							/>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>Settings</DropdownMenuItem>
+						<DropdownMenuItem>Support</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>Logout</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+        </header>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		// This ensures that only external images from fakestoreapi can be served from the Next.js Image Optimization API.
+		// This ensures that only external images from fakestoreapi or imgur can be served from the Next.js Image Optimization API.
 		// Any other protocol, hostname, port, or unmatched path will respond with 400 Bad Request.
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "fakestoreapi.com",
+				port: "",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
 				port: "",
 				pathname: "**",
 			},
