42ded0a7db78472cd792c9d88bdf73505cc7ecc5
issam-seghir <issamusma@hotmail.com>
2024-05-17 23:29:06 +0100

    styling grid section

---
+++ components/grid-section.tsx
@@ -0,0 +1,74 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function GridSection() {
+    return (
+        <section className="flex flex-auto items-center justify-center p-4 sm:p-8">
+            <div className="grid gap-4 lg:grid-cols-2">
+                <div className="bg-background-secondary flex flex-row gap-4 overflow-hidden rounded-lg">
+                    <Image
+                        src={
+                            "https://assets.lummi.ai/assets/QmXssdq8hp1V3F9XfJMgkrkoRu7MbsKoGxYn5wCtu5KVKd?auto=format&w=1500"
+                        }
+                        alt="Fashion Girl"
+                        loading="lazy"
+                        width={250}
+                        height={200}
+                        className="w-36 transition-all sm:w-52 md:w-64"
+                    />
+                    <div className="flex flex-auto flex-col items-start justify-between gap-2 p-4 md:p-8">
+                        <div className="space-y-2">
+                            <h1 className="text-base font-bold sm:text-2xl md:text-3xl">
+                                Women Collection
+                            </h1>
+                            <h2 className="text-sm font-semibold sm:text-lg md:text-xl">
+                                Collection
+                            </h2>
+                            <p className="line-clamp-2 text-xs sm:text-base">
+                                Explore our empowering Women&apos;s Collection,
+                                celebrating grace and confidence.{" "}
+                            </p>
+                            <p className="line-clamp-2 text-xs sm:text-base">
+                                Shop now and elevate your wardrobe with
+                                fashion-forward ensembles!
+                            </p>
+                        </div>
+                        <Button size="sm" className="">
+                            <Link
+                                href="/products"
+                                className="text-xs sm:text-sm"
+                            >
+                                See products
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="grid grid-rows-[150px_200px] gap-4 lg:grid-cols-2">
+                    <div className="bg-background-secondary flex w-full flex-row gap-4 rounded-lg bg-purple-100">
+                        <div className="m-8 flex flex-auto flex-col justify-center">
+                            <h2 className="text-base font-semibold text-black">
+                                24 Items
+                            </h2>
+                            <h1 className="text-xl font-bold text-black sm:text-3xl">
+                                Betsellers
+                            </h1>
+                        </div>
+                    </div>
+
+                    <div className="bg-background-secondary rounded-lg bg-[url('https://assets.lummi.ai/assets/QmdrZiVdZQmzxUhiBafTorGGXZfAvwDqBZHoikXVjxJXNk?auto=format&w=1500')] bg-cover bg-center bg-no-repeat">
+                        <h1 className="ml-4 mt-16 inline-flex rounded bg-white bg-opacity-25 px-4 py-1 text-lg font-bold text-white">
+                            New
+                        </h1>
+                        <h2 className="ml-4 text-xl font-semibold text-white">
+                            Collection
+                        </h2>
+                    </div>
+
+                    <div className="bg-background-secondary col-span-2 min-h-min w-full rounded-lg bg-[url('https://assets.lummi.ai/assets/QmdsXvqRHNBdUJAZeRm8mb3tkwxuxx4ZJpgXtoZRki763k?auto=format&w=1500')] bg-cover bg-top bg-no-repeat"></div>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ components/hero-section.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function HeroSection() {
+    return (
+        <section className="block overflow-x-hidden md:h-[45hv] lg:h-[55hv] xl:h-[70vh]">
+            <article className="relative flex h-auto max-h-full w-auto max-w-full flex-row">
+                <Image
+                    loading="eager"
+                    width={1400}
+                    height={500}
+                    priority={true}
+                    src="https://assets.lummi.ai/assets/QmXarCVzUC6q6QGL56JJkWW7NucjkDrP4q6nsVQ72yzJ7q?auto=format&w=1400"
+                    alt="Girl Rockstar"
+                    className="h-auto max-h-full max-w-full object-cover"
+                />
+                <Image
+                    loading="eager"
+                    width={600}
+                    height={500}
+                    priority={true}
+                    src="https://assets.lummi.ai/assets/QmS8NLeQhVFxpcJZpKevcvgXUdhFmKSqA3yh9KcfLP7Lkg?auto=format&w=600"
+                    alt="Girl Rockstar"
+                    className="h-auto max-h-full max-w-full object-cover"
+                />
+
+                <div className="absolute flex h-full w-full flex-col items-center justify-center gap-4 ">
+                    <h1 className=" dark:animate-text bg-gradient-to-t from-purple-400 via-purple-600  to-blue-500 font-black  dark:bg-gradient-to-r dark:from-teal-500 dark:to-orange-500 bg-clip-text text-transparent md:text-7xl lg:text-9xl">
+                        Spectrum Store
+                    </h1>
+
+                    <h2
+                        className="text- rounded-sm px-8 py-2 text-base  font-bold lg:px-24 lg:text-3xl"
+                        style={{ color: "rgb(248, 250, 252)" }}
+                    >
+                        Your One-Stop Shop
+                    </h2>
+                    <Link
+                        href="/products"
+                        className={buttonVariants({ variant: "outline" })}
+                    >
+                        See All Products
+                    </Link>
+                </div>
+            </article>
+        </section>
+    );
+}
