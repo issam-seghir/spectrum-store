717391c0aa0a3d7f6ac26a2f2ad3abb2a794201e
issam-seghir <issamusma@hotmail.com>
2024-05-17 10:56:39 +0100

    init project with shadcn + some config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		// This ensures that only external images from fakestoreapi can be served from the Next.js Image Optimization API.
+		// Any other protocol, hostname, port, or unmatched path will respond with 400 Bad Request.
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "fakestoreapi.com",
+				port: "",
+				pathname: "**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
