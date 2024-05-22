a75a926e476184b5d95b8d91dba69842ce0660a5
issam-seghir <issamusma@hotmail.com>
2024-05-22 18:54:27 +0100

    fix dal.ts types error

--- lib/dal.ts
+++ lib/dal.ts
@@ -10,8 +10,10 @@ const ADMIN_ROLE : number = 2;
 
 export const verifySession = cache(async () => {
     const token = cookies().get("token")?.value;
-    const decoded = jwt.decode(token);
-
+    let decoded: any = null;
+    if (token) {
+        decoded = jwt.decode(token);
+    }
     // Redirect to login page if the user is not authenticated
     if (!decoded?.user) {
         redirect("/login");
